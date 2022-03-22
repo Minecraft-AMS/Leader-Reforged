865e65529ea1176599ee22981cad0620f838b8d1
XavierWah <xavierwah@outlook.com>
2022-03-22 08:12:53 +0800

    Release 1.0.3

--- leader_reforged/__init__.py
+++ leader_reforged/__init__.py
@@ -1,35 +1,28 @@
 import json
-from mcdreforged.api.all import *
 import os
 
+from mcdreforged.api.all import *
+
 leaders = []
-players = []
 config = {"max_leaders": 1024}
 
 
-def leader(server: ServerInterface, info: Info, player: str = None):
-    global leaders, players, config
-    controlled = False
-    if player is None:
-        player = info.player
-    else:
-        # 临时取消了玩家识别 等待更好的方案
-        # if player not in players:
-        #     server.reply(info, RText(server.rtr("leader_reforged.error.player_not_found"), RColor.red))
-        #     return
-        controlled = True
-    if info.player == player:
-        controlled = False
+def leader(server: ServerInterface, executor: str, player: str = None):
+    global leaders, config
+    controlled = False if player is None or executor == player else True
+    if not controlled:
+        player = executor
+    # !!! 此处缺少在线玩家检测 等待更好的方法
     if player not in leaders:
         if len(leaders) >= config["max_leaders"]:
-            server.reply(
-                info,
+            server.tell(
+                executor,
                 RText(
                     server.rtr("leader_reforged.error.max_limit_reached"), RColor.red
                 ),
             )
-            server.reply(
-                info,
+            server.tell(
+                executor,
                 RText(
                     server.rtr("leader_reforged.usage.leaders", " ".join(leaders)),
                     RColor.red,
@@ -47,8 +40,8 @@ def leader(server: ServerInterface, info: Info, player: str = None):
             ),
         )
         if controlled:
-            server.reply(
-                info,
+            server.tell(
+                executor,
                 RText(
                     server.rtr("leader_reforged.usage.mark", player),
                     RColor.green,
@@ -67,26 +60,13 @@ def leader(server: ServerInterface, info: Info, player: str = None):
             ),
         )
         if controlled:
-            server.reply(
-                info,
+            server.tell(
+                executor,
                 RText(
                     server.rtr("leader_reforged.usage.unmark", player),
                     RColor.green,
                     RStyle.italic,
                 ),
-            )
-
-
-def on_user_info(server: PluginServerInterface, info: Info):
-    raw_command = info.content.split()
-    if raw_command[0] == "!!leader":
-        if len(raw_command) == 1:
-            leader(server, info)
-        elif len(raw_command) == 2:
-            leader(server, info, raw_command[1])
-        else:
-            server.reply(
-                info, RText(server.rtr("leader_reforged.error.usage"), RColor.red)
             )
 
 
@@ -98,18 +78,16 @@ def on_load(server: PluginServerInterface, old):
     except FileNotFoundError:
         with open(os.path.join(server.get_data_folder(), "config.json"), "w") as file:
             json.dump(config, file)
+    server.register_command(
+        Literal("!!leader")
+        .requires(lambda src: src.is_player)
+        .runs(lambda src: leader(server, src.player))
+        .then(
+            Text("player").runs(
+                lambda src, ctx: leader(server, src.player, ctx["player"])
+            )
+        )
+    )
     server.register_help_message(
         "!!leader", server.rtr("leader_reforged.plugin.help_message")
     )
-
-
-# def on_player_joined(server: PluginServerInterface, player: str, info: Info):
-#     global players
-#     players.append(player)
-#
-# def on_player_left(server: PluginServerInterface, player: str):
-#     global players
-#     try:
-#         players.remove(player)
-#     except Exception:
-#         pass
